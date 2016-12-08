f96ae9a9742fb69feefc3343b41071a64ff3b52d
Tom Fuller <tomf95@hotmail.co.uk>
2016-12-08 12:15:22 +0000

    current weather added

--- interface.js
+++ interface.js
@@ -3,6 +3,7 @@ $(document).ready(function()
   var thermostat = new Thermostat();
 
   updateTemperature();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0f442b2b2f3666605e59b18c9a384324&units=metric', function(data) { console.log(data.main.temp); })
 
   $('#up').on('click', function() {
     thermostat.up();
@@ -28,6 +29,13 @@ $(document).ready(function()
     thermostat.turnSavePowerOff();
     updateTemperature();
   });
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',uk&appid=0f442b2b2f3666605e59b18c9a384324&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  })
 
   function updateTemperature() {
     $('#temperature').text(thermostat._temperature);
